478df5be1736e9134c6f1bbb33db04c3672acd12
Nick Mirambeau <nickmirambeau@gmail.com>
2026-06-14 01:14:46 -0400

    Update and rename 63166096.json to 63166096.js

---
+++ Scripts/2-Spells/63166096.js
@@ -0,0 +1,54 @@
+var archetype = "Sky Striker";
+var addLabel = "Add 1 \"$archetype\" card from your Deck to your hand";
+var condition = "(NAME == %$archetype% || DESC == %This card is always treated as a \"$archetype\" card%) && NAME != $name";
+var search = "search(0,255,[0],\"$condition\")";
+
+"commands":[
+  {
+    "conditions":[
+      {
+        "AND":[
+          {
+            "control_self":"$condition",
+            "zone":0
+          },
+          {
+            "NOT":[
+              {
+                "control_self":0,
+                "zone_range":[10,14]
+              },
+              {
+                "bool":"{count(2, \"ATTR == spell\")} >= 3"
+              }
+            ]
+          }
+        ]
+      },
+      {
+        "AND":[
+          {
+            "control_self":"$condition",
+            "zone":0
+          },
+          {
+            "bool":"{count(2, \"ATTR == spell\")} >= 3"
+          },
+          {
+            "NOT":[
+              {
+                "control_self":0,
+                "zone_range":[10,14]
+              }
+            ]
+          }
+        ]
+      }
+    ],
+    "trigger_range":[20,24],
+    "options":[
+      "label=$addLabel ONLY¬condition=0¬$search",
+      "label=$addLabel, then draw 1 card¬condition=1¬$search¬then¬draw(1)"
+    ]
+  }
+]
