873850e9817c76e9b1d3929a69dbbf65c0385abd
Nick Mirambeau <nickmirambeau@gmail.com>
2026-06-14 02:20:51 -0400

    Update 63166095.js

--- Scripts/2-Spells/63166095.js
+++ Scripts/2-Spells/63166095.js
@@ -1,6 +1,6 @@
 var archetype = "Sky Striker";
 var addLabel = "Add 1 \"$archetype\" card from your Deck to your hand";
-var condition = "NAME != $name && (NAME == %$archetype% || DESC == %$archetype\" card%)";
+var condition = "NAME != $name && (NAME == %$archetype% || DESC == %This card is always treated as a \\\"$archetype\\\" card%)";
 var search = "search(0,255,[0],\"$condition\")";
 
 "commands":[
