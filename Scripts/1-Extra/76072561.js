af80b430fb3ec2316a3ef0e3fbbb8d9c7e46e34c
Nick Mirambeau <nickmirambeau@gmail.com>
2026-06-14 00:20:15 -0400

    Update 76072561.js

--- Scripts/1-Extra/76072561.js
+++ Scripts/1-Extra/76072561.js
@@ -1,16 +1,26 @@
 var archetype = "Sky Striker Ace -";
-var searchCondition = "NAME == $archetype Raye || NAME == $archetype Roze";
+var rayeCondition = "NAME == $archetype Raye";
+var rozeCondition = "NAME == $archetype Roze";
+var bothConditions = "$rayeCondition || $rozeCondition"
 
 "commands":[
     {
         "conditions":{
-            "control_self":"$searchCondition",
-            "zone_list":[0,2]
+            "AND":[
+                {
+                    "control_self":"$rayeCondition",
+                    "zone_list":[0,2]
+                },
+                {
+                    "control_self":"$rozeCondition",
+                    "zone_list":[0,2]
+                }
+            ]
         },
         "trigger":2,
         "msg":"Resolve $name's Quick Effect?",
         "options":[
-            "label=Special Summon 1 \"$archetype Raye\" and 1 \"$archetype Roze\" from your Deck and/or GY¬search(0,255,[0,2],\"$searchCondition\")¬then¬break_if(\"$num_searched < 2\")¬search(0,255,[0,2],\"$searchCondition\")"
+            "label=Special Summon 1 \"$archetype Raye\" and 1 \"$archetype Roze\" from your Deck and/or GY¬search(0,255,[0,2],\"$bothConditions\")¬then¬break_if(\"$num_searched < 2\")¬search(0,255,[0,2],\"$bothConditions\")"
         ]
     }
 ]
