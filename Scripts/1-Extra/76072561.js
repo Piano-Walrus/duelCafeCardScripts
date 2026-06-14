d50c2dcd8b81a1b4761d7c1649601d282c9469d2
Nick Mirambeau <nickmirambeau@gmail.com>
2026-06-14 16:43:17 -0400

    Update 76072561.js

--- Scripts/1-Extra/76072561.js
+++ Scripts/1-Extra/76072561.js
@@ -4,6 +4,7 @@ var rayeCondition = "NAME == $aceRaye";
 var rozeCondition = "NAME == $aceRoze";
 var bothConditions = "$rayeCondition || $rozeCondition";
 var hasSpellCondition = "ATTR == spell && NAME == %$archetype%";
+var ternaryLeftHalf = "($name_searched == $aceRaye && {count($location_searched,$rozeCondition)} < 1) || ($name_searched == $aceRoze && {count($location_searched,$rayeCondition)} < 1) ? {|$locationSearched - 2|}";
 
 "commands":[
     {
@@ -25,7 +26,7 @@ var hasSpellCondition = "ATTR == spell && NAME == %$archetype%";
         "trigger":2,
         "msg":"Resolve $name's Quick Effect?",
         "options":[
-            "label=Special Summon 1 \"$aceRaye\" and 1 \"$aceRoze\" from your Deck and/or GY¬debug¬search(0,255,[0,2],\"$bothConditions\")¬then¬break_if(\"$num_searched != 1\")¬search({($name_searched == $aceRaye && {count($location_searched,$rozeCondition)} < 1) || ($name_searched == $aceRoze && {count($location_searched,$rayeCondition)} < 1) ? {|$locationSearched - 2|}},255,[255],\"{$name_searched == $aceRaye ? $rozeCondition : $rayeCondition}\")"
+            "label=Special Summon 1 \"$aceRaye\" and 1 \"$aceRoze\" from your Deck and/or GY¬debug¬search(0,255,[0,2],\"$bothConditions\")¬then¬break_if(\"$num_searched != 1\")¬search({$ternaryLeftHalf : $location_searched},255,[{$ternaryLeftHalf : 0,2}],\"{$name_searched == $aceRaye ? $rozeCondition : $rayeCondition}\")"
         ]
     },
     {
