c60c258cd5b9bb44bfd82fd365cafa667397b5a4
Nick Mirambeau <nickmirambeau@gmail.com>
2026-06-13 22:00:21 -0400

    Update and rename 54143349.json to 54143349.js

---
+++ Scripts/0-Main/54143349.js
@@ -0,0 +1,17 @@
+var archetype = "Radiant Typhoon";
+var mst = "Mystical Space Typhoon";
+var searchCondition = "(IS_MONSTER && NAME == %$archetype% && NAME != $name) || NAME == $mst";
+
+"commands":[
+    {
+        "conditions":{
+            "control_self":"$searchCondition",
+            "zone":0
+        },
+        "trigger_range":[10,14],
+        "msg":"Resolve $name's on-summon effect?",
+        "options":[
+            "label=Add 1 \"$archetype\" monster (except \"$name\") or 1 \"$mst\" from your Deck to your hand¬search(0,255,[0],\"$searchCondition\")"
+        ]
+    }
+]
