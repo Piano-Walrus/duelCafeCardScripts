572ad9f6cd278dd4e3ed5168e8fef5e434afbf56
Nick Mirambeau <nickmirambeau@gmail.com>
2026-06-14 02:51:32 -0400

    Update 65889305.js

--- Scripts/3-Traps/65889305.js
+++ Scripts/3-Traps/65889305.js
@@ -11,16 +11,12 @@ var searchCondition = "NAME == %$archetype% && NAME != $name";
                 {
                     "control_self":"$searchCondition",
                     "zone":0
-                },
-                {
-                    "control_self":0,
-                    "zone_range":[2,3]
                 }
             ]
         },
         "trigger_range":[10,14],
         "options":[
-            "label=Add 1 \"$archetype\" card from your Deck to your hand, except \"$archetype - Lightning Tydeu\"¬search(0,255,[0],\"$searchCondition\")"
+            "label=Add 1 \"$archetype\" card from your Deck to your hand, except $name¬search(0,255,[0],\"$searchCondition\")"
         ]
     }
 ]
