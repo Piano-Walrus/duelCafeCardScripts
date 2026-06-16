fe848c524b86cfc7d161a54615aee895fda1109a
Nick Mirambeau <nickmirambeau@gmail.com>
2026-06-16 03:36:13 -0400

    Update 84550369.js

--- Scripts/2-Spells/84550369.js
+++ Scripts/2-Spells/84550369.js
@@ -10,7 +10,7 @@ const setCondition = "IS_SPELL_OR_TRAP && $sendCondition";
         },
         "trigger_range":[20,24],
         "options":[
-            "label=Send 1 \"$archetype\" card from your Deck to the GY, except \"$name\", then you can Set 1 \"$archetype\" Spell/Trap from your Deck, except \"$name\"¬search(0,2,[0],\"$sendCondition\",-1,2)¬then¬break_if(\"{count(0,\\\"$setCondition\\\")} < 1\")¬search(0,255,[0],\"$setCondition\")"
+            "label=Send 1 \"$archetype\" card from your Deck to the GY, except \"$name\", then you can Set 1 \"$archetype\" Spell/Trap from your Deck, except \"$name\"¬search(0,2,[0],\"$sendCondition\",-1,2)¬then¬break_if(\"{count(0,\"$setCondition\")} < 1\")¬search(0,255,[0],\"$setCondition\")"
         ]
     }
 ]
