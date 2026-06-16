3ea9b9674dc4d880fd30df66a569181b84f840b1
Nick Mirambeau <nickmirambeau@gmail.com>
2026-06-15 22:12:01 -0400

    Update and rename 30432463.json to 30432463.js

---
+++ Scripts/0-Main/30432463.js
@@ -0,0 +1,17 @@
+const archetype = "Vaalmonica";
+
+"commands":[
+    {
+        "msg":"Resolve $name's effect?",
+        "trigger_range":[23,24],
+        "options":[
+            "label=Scale \"Angello $archetype\" in your other Pendulum Zone¬source=0¬dest={|{{$trigger-23}-1}|+23}¬target=3048768¬place=1"
+        ]
+    },
+    {
+        "msg":"Resolve $name's ignition effect?",
+        "options":[
+            "label=Banish 1 \"$archetype\" Normal Spell/Trap from your GY¬search(2,3,[2],\"IS_SPELL_OR_TRAP && NAME == %$archetype% && FRAME == %normal%\",-1,20,false)"
+        ]
+    }
+]
