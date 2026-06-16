15a38f878cbc82a7ab7aa411d3e0a1e10fcc499a
Nick Mirambeau <nickmirambeau@gmail.com>
2026-06-15 22:39:47 -0400

    Update and rename 23093373.json to 23093373.js

---
+++ Scripts/0-Main/23093373.js
@@ -0,0 +1,16 @@
+const archetype = "Vaalmonica";
+const searchCondition = "NAME == %$archetype% && NAME != $name";
+
+"commands":[
+    {
+        "conditions":{
+            "control_self":"$searchCondition",
+            "zone":0
+        },
+        "trigger_range":[10,14],
+        "msg":"Resolve $name's on-summon effect?",
+        "options":[
+            "label=Add 1 \"$archetype\" card from your Deck to your hand, except \"$name\"¬search(0,255,[0],\"$searchCondition\")"
+        ]
+    }
+]
