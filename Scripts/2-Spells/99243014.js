be0afe69285507e2feacd9975a8e4a9dc7ae5756
Nick Mirambeau <nickmirambeau@gmail.com>
2026-06-17 13:34:25 -0400

    Update 99243014.js

--- Scripts/2-Spells/99243014.js
+++ Scripts/2-Spells/99243014.js
@@ -11,14 +11,17 @@ const isSynchro = "TYPES == synchro";
             {
                 "label":"Reveal 1 Synchro Monster in your Extra Deck, then add to your hand or Special Summon 1 of the Synchro Materials mentioned on it from your Deck or GY",
                 "script":{
-                    search(1,255,[1],"$isSynchro && DESC == \"% && DESC == %\" + %");
+                    search(1,1,[1],"$isSynchro && DESC == \"% && DESC == %\" + %");
                     then;
                     $revealedCard=$name_searched;
+                    source=1;
+                    dest=7;
+                    excavate_until("NAME == $revealedCard");
                     break_if("$num_searched != 1");
                     break_if("{count(0, \"$desc_searched == \\\"NAME\\\" + %\")} < 1");
                     search(0,255,[0,2],"$desc_searched == \"NAME\" + %");
                     then;
-                    source=6;
+                    source=7;
                     dest=1;
                     excavate_until("NAME == $revealedCard");
                 }
