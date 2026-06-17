02e552c4bc3c3600ada5c28859f824d2b79b0211
Nick Mirambeau <nickmirambeau@gmail.com>
2026-06-17 04:06:25 -0400

    Create 99243014.js

---
+++ Scripts/2-Spells/99243014.js
@@ -0,0 +1,26 @@
+const revealCondition = "TYPES == synchro && DESC == \"% && DESC == %\" + %";
+
+"commands":[
+    {
+        "conditions":{
+            "control_self":"$revealCondition",
+            "zone":1
+        },
+        "trigger_range":[20,24],
+        "options":[
+            {
+                "label":"Reveal 1 Synchro Monster in your Extra Deck, then add to your hand or Special Summon 1 of the Synchro Materials mentioned on it",
+                "script":{
+                    search(1,255,[1],"$revealCondition");
+                    $revealedCard="$name_searched";
+                    break_if("$num_searched != 1");
+                    break_if("{count(0, \"$desc_searched == \\\"NAME\\\" + %\")} < 1");
+                    search(0,255,[0],"$desc_searched == \"NAME\" + %");
+                    source=6;
+                    dest=1;
+                    excavate_until("NAME == $revealedCard");
+                }
+            }
+        ]
+    }
+]
