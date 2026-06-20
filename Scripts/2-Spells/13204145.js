a9053c5ce966e8f1985ad36a1ecb162f6343edcc
Nick Mirambeau <nickmirambeau@gmail.com>
2026-06-19 21:38:46 -0400

    Create 13204145.js

---
+++ Scripts/2-Spells/13204145.js
@@ -0,0 +1,8 @@
+"commands":[
+    {
+        "trigger_range":[20,24],
+        "options":[
+            "label=Reveal 2 Flip monsters with different names from your Deck¬search(0,255,[0],\"IS_MONSTER && TYPES == flip\")"
+        ]
+    }
+]
